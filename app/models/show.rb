f69638e26a97082da457616a5bf2a38b8dd50024
Garrett <Garrett.Bodley@gmail.com>
2020-11-24 18:23:15 -0500

    commit tree 2676948037992ec41465e48d1889afa7d46c121f

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def build_network(hash)
+    Network.where(hash)[0] ? Network.where(hash)[0].tap {|network| network.shows << self} : Network.create(hash).tap {|network| network.shows << self}
+  end
+
+  def actors_list
+    Character.where(show: self).map{|character| character.actor.full_name}.uniq
+  end
 end
