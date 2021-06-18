fb38baf569f2e0f65d9182ed7f124086a60f47af
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-18 14:30:05 -0400

    Archiving Flatiron Code.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,18 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+  belongs_to :network
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def build_show(hash)
+      Show.where(hash)[0] ? Show.where(hash)[0].tap {|show| show.characters << self} : Show.create(hash).tap {|show| show.characters << self}
+  end
+
+  def build_network(hash)
+    Network.where(hash)[0] ? Network.where(hash)[0].characters << self : Network.create(hash).characters << self
+  end
+
 end
